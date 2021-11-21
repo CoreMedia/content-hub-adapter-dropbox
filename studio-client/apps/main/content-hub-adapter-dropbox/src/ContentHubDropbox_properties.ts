0a586bb3cbfd07c31b63cb68849093c176ee6c82
Philipp Hofmann <philipp.hofmann@coremedia.com>
2021-11-21 20:09:51 +0100

    Prep for CM11 release. Now using svg from workspace

--- studio-client/apps/main/content-hub-adapter-dropbox/src/ContentHubDropbox_properties.ts
+++ studio-client/apps/main/content-hub-adapter-dropbox/src/ContentHubDropbox_properties.ts
@@ -1,4 +1,6 @@
 import CoreIcons_properties from "@coremedia/studio-client.core-icons/CoreIcons_properties";
+import icon from "./icons/dropbox_24.svg";
+import SvgIconUtil from "@coremedia/studio-client.cap-base-models/util/SvgIconUtil";
 
 /**
  * Interface values for ResourceBundle "ContentHubDropbox".
@@ -36,7 +38,7 @@ const ContentHubDropbox_properties: ContentHubDropbox_properties = {
   folder_type_dbx_folder_name: "Folder",
   folder_type_dbx_folder_icon: CoreIcons_properties.folder,
   adapter_type_dropbox_name: "Dropbox",
-  adapter_type_dropbox_icon: CoreIcons_properties.dropbox,
+  adapter_type_dropbox_icon: SvgIconUtil.getIconStyleClassForSvgIcon(icon),
   item_type_dbx_file_name: "File",
   item_type_dbx_file_icon: CoreIcons_properties.type_external_content,
   metadata_sectionName: "Metadata",
